552f5c53772304c0086f15b59e1500cc690b3e16
MarcDysart <dysart.marc@gmail.com>
2014-11-16 20:27:43 -0700

    Animate side-nav

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,4 +1,29 @@
-$('.menu1').on('click', function (e) {
+
+
+// $('.navbar-brand').on('click'), function (f) {
+//   f.preventDefault();
+//   $('.side-nav').css( "width", "100%");
+// });
+
+// $('.side-nav').on('click', function (e) {
+//   e.preventDefault();
+//   $( this ).css( "width", "25%" );
+//   $( '.side-nav>li>a' ).css( {"width": "500px",
+//   "text-align": "left"});
+//
+// });
+
+$('.side-nav').on('click', function (e) {
   e.preventDefault();
-  $('.side-nav').toggleClass('clicked');
+  $( this ).css( "width", "300px" );
+  $( ".side-nav>li>a" ).animate({
+      width: "300px",
+      "text-align": "left"
+    },250 );
+  $( "#wrapper" ).animate({
+      "padding-left": "325px"
+    },250 );
+
+
+
 });
