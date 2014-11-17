d671030ea6ce0bb439cefcb54db08ca64eae0e6d
MarcDysart <dysart.marc@gmail.com>
2014-11-16 21:54:00 -0700

    Set Correct Menu names, changed font size, fixed nav-bar motion

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,29 +1,17 @@
 
-
-// $('.navbar-brand').on('click'), function (f) {
-//   f.preventDefault();
-//   $('.side-nav').css( "width", "100%");
-// });
-
-// $('.side-nav').on('click', function (e) {
-//   e.preventDefault();
-//   $( this ).css( "width", "25%" );
-//   $( '.side-nav>li>a' ).css( {"width": "500px",
-//   "text-align": "left"});
-//
-// });
+// Animates the Navbar
 
 $('.side-nav').on('click', function (e) {
   e.preventDefault();
-  $( this ).css( "width", "300px" );
+  $( this ).css( "width", "200px" );
   $( ".side-nav>li>a" ).animate({
-      width: "300px",
-      "text-align": "left"
-    },250 );
-  $( "#wrapper" ).animate({
-      "padding-left": "325px"
-    },250 );
+      width: "200px",
+      "font-size": "1em"
+    },50);
 
+  $( "#wrapper" ).animate({
+      "padding-left": "225px"
+    },1500 );
 
 
 });
